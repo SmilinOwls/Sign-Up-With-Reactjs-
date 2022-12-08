109cacf287c04fa3490a53787bebb9a141803d88
20120234 <20120234@student.hcmus.edu.vn>
2022-12-08 16:23:46 +0700

    Build detail

---
+++ build/precache-manifest.edc9cb1f0d856463c4bed1251043806f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "5112c1149ffbde636b4c",
+    "url": "/Sign-Up-With-Reactjs-/static/css/main.d60de3b1.chunk.css"
+  },
+  {
+    "revision": "5112c1149ffbde636b4c",
+    "url": "/Sign-Up-With-Reactjs-/static/js/main.5112c114.chunk.js"
+  },
+  {
+    "revision": "f5d6f27f7efdfbf308dc",
+    "url": "/Sign-Up-With-Reactjs-/static/js/1.f5d6f27f.chunk.js"
+  },
+  {
+    "revision": "1013804c505b11bfdb56",
+    "url": "/Sign-Up-With-Reactjs-/static/js/runtime~main.1013804c.js"
+  },
+  {
+    "revision": "99305dd333dc312d125c63be426e51eb",
+    "url": "/Sign-Up-With-Reactjs-/static/media/registration-form-4.99305dd3.jpg"
+  },
+  {
+    "revision": "a2fc08a54a46b0663a084ce4ba19cce2",
+    "url": "/Sign-Up-With-Reactjs-/static/media/ElMessiri-SemiBold.a2fc08a5.ttf"
+  },
+  {
+    "revision": "c88cecbffad6d8e731fd95de49561ebd",
+    "url": "/Sign-Up-With-Reactjs-/static/media/Montserrat-SemiBold.c88cecbf.ttf"
+  },
+  {
+    "revision": "9c46095118380d38f12e67c916b427f9",
+    "url": "/Sign-Up-With-Reactjs-/static/media/Montserrat-Regular.9c460951.ttf"
+  },
+  {
+    "revision": "16bda8c7b22c4ae29317c618a31cde58",
+    "url": "/Sign-Up-With-Reactjs-/index.html"
+  }
+];
