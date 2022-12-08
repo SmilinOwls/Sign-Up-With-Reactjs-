ae0abcb3a27a8f3b2eeb1e28993fb4a88cebfe74
20120234 <20120234@student.hcmus.edu.vn>
2022-12-08 17:10:52 +0700

    fix responsive

--- src/App.js
+++ src/App.js
@@ -11,9 +11,9 @@ const phoneRegExp = /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2
 
 function App(props) {
 
-  const [user,setUser] = useState(props.user || {});
+  const [user, setUser] = useState(props.user || {});
   const [score, setScore] = useState(props.score || 0);
-  const [pwBtn,setPwBtn] = useState(props.pwBtn || 'show');
+  const [pwBtn, setPwBtn] = useState(props.pwBtn || 'show');
   const [type, setType] = useState(props.type || 'password');
 
   // constructor(props) {
@@ -64,20 +64,20 @@ function App(props) {
   }
 
   const handleChange = (event) => {
-    const {name, value} = event.target;
+    const { name, value } = event.target;
 
     setUser(user => ({
       ...user,
-      [name] : value
+      [name]: value
     }));
   }
 
   const pwHandleChange = (event) => {
-    const {name, value} = event.target;
-    
+    const { name, value } = event.target;
+
     setUser(user => ({
       ...user,
-      [name] : value
+      [name]: value
     }));
 
     if (event.target.value === "") {
@@ -94,136 +94,144 @@ function App(props) {
     setPwBtn(pwBtn === "show" ? "hide" : "show");
   }
 
-  const handleSubmit = (data) => {
-    // Code in server goes here
-    console.log(JSON.stringify({user: user, score: score, pwBtn: pwBtn, type: type}, null, 2));
+  const resetFormData = () => {
+    setUser({});
+    setScore(0);
+    setPwBtn("");
+    setType("");
   }
 
-  
-    return (
-      <div className="inner">
-        <div className="image-holder">
-          <img src={SignUpImage} alt="" />
-        </div>
-        <div className='form'>
-          <h3>Sign Up</h3>
-          <Formik
-            initialValues={user}
-            validationSchema={validationSchema}
-            onSubmit={handleSubmit}
-          >
-            {({ errors, touched, resetForm }) => (
+  const handleSubmit = (data) => {
+    // Code in server goes here
+    console.log(JSON.stringify({ user: user, score: score, pwBtn: pwBtn, type: type }, null, 2));
+  }
 
-              <Form>
-                <div className="form-holder active">
-                  <Field
-                    name="username"
-                    type="text"
-                    className={
-                      'form-control' +
-                      (errors.username && touched.username ? ' is-invalid' : '')
-                    }
-                    placeholder="USERNAME"
-                    onKeyUp={handleChange}
-                  />
-                  <ErrorMessage
-                    name="username"
-                    component="div"
-                    className="invalid-feedback"
-                  />
-                </div>
 
-                <div className="form-holder">
+  return (
+    <div className="inner">
+      <div className="image-holder">
+        <img src={SignUpImage} alt="" />
+      </div>
+      <div className='form'>
+        <h3>Sign Up</h3>
+        <Formik
+          initialValues={user}
+          validationSchema={validationSchema}
+          onSubmit={handleSubmit}
+        >
+          {({ errors, touched, resetForm }) => (
 
-                  <Field
-                    name="email"
-                    type="email"
-                    className={
-                      'form-control' +
-                      (errors.email && touched.email ? ' is-invalid' : '')
-                    }
-                    placeholder="E-MAIL"
-                    onKeyUp={handleChange}
-                  />
-                  <ErrorMessage
-                    name="email"
-                    component="div"
-                    className="invalid-feedback"
-                  />
-                </div>
+            <Form>
+              <div className="form-holder active">
+                <Field
+                  name="username"
+                  type="text"
+                  className={
+                    'form-control' +
+                    (errors.username && touched.username ? ' is-invalid' : '')
+                  }
+                  placeholder="USERNAME"
+                  onKeyUp={handleChange}
+                />
+                <ErrorMessage
+                  name="username"
+                  component="div"
+                  className="invalid-feedback"
+                />
+              </div>
 
-                <div className="form-holder">
+              <div className="form-holder">
 
-                  <Field
-                    name="phone"
-                    type="text"
+                <Field
+                  name="email"
+                  type="email"
+                  className={
+                    'form-control' +
+                    (errors.email && touched.email ? ' is-invalid' : '')
+                  }
+                  placeholder="E-MAIL"
+                  onKeyUp={handleChange}
+                />
+                <ErrorMessage
+                  name="email"
+                  component="div"
+                  className="invalid-feedback"
+                />
+              </div>
 
-                    className={
-                      'form-control' +
-                      (errors.phone && touched.phone ? ' is-invalid' : '')
-                    }
-                    placeholder="PHONE"
-                    onKeyUp={handleChange}
-                  />
-                  <ErrorMessage
-                    name="phone"
-                    component="div"
-                    className="invalid-feedback"
-                  />
-                </div>
+              <div className="form-holder">
 
-                <div className="form-holder">
+                <Field
+                  name="phone"
+                  type="text"
 
-                  <Field
-                    name="password"
-                    type={type}
-                    className={
-                      'form-control' +
-                      (errors.password && touched.password ? ' is-invalid' : '')
-                    }
-                    placeholder="PASSWORD"
-                    onKeyUp={pwHandleChange}
+                  className={
+                    'form-control' +
+                    (errors.phone && touched.phone ? ' is-invalid' : '')
+                  }
+                  placeholder="PHONE"
+                  onKeyUp={handleChange}
+                />
+                <ErrorMessage
+                  name="phone"
+                  component="div"
+                  className="invalid-feedback"
+                />
+              </div>
 
-                  />
+              <div className="form-holder">
 
-                  <ErrorMessage
-                    name="password"
-                    component="div"
-                    className="invalid-feedback"
-                  />
-                </div>
+                <Field
+                  name="password"
+                  type={type}
+                  className={
+                    'form-control' +
+                    (errors.password && touched.password ? ' is-invalid' : '')
+                  }
+                  placeholder="PASSWORD"
+                  onKeyUp={pwHandleChange}
 
-                <div className='pwStrRow'>
-                  {score >= 1 && <div>
-                    <PasswordStr score={score} />
-                    <button
-                      className="btn btn-success my-2 pwBtn"
-                      onClick={pwMask}
-                    >{pwBtn}</button>
-                  </div>}
-                </div>
+                />
 
-                <div className="form-holder">
+                <ErrorMessage
+                  name="password"
+                  component="div"
+                  className="invalid-feedback"
+                />
+              </div>
 
-                  <Field
-                    name="confirmPassword"
-                    type={type}
-                    className={
-                      'form-control' +
-                      (errors.confirmPassword && touched.confirmPassword
-                        ? ' is-invalid'
-                        : '')
-                    }
-                    placeholder="RE-PASSWORD"
-                    onKeyUp={handleChange}
-                  />
-                  <ErrorMessage
-                    name="confirmPassword"
-                    component="div"
-                    className="invalid-feedback"
-                  />
-                </div>
+              <div className='pwStrRow'>
+                {score >= 1 && <div>
+                  <PasswordStr score={score} />
+                  <button
+                    className="btn btn-success my-2 pwBtn"
+                    onClick={pwMask}
+                  >{pwBtn}</button>
+                </div>}
+              </div>
 
+              <div className="form-holder">
+
+                <Field
+                  name="confirmPassword"
+                  type={type}
+                  className={
+                    'form-control' +
+                    (errors.confirmPassword && touched.confirmPassword
+                      ? ' is-invalid'
+                      : '')
+                  }
+                  placeholder="RE-PASSWORD"
+                  onKeyUp={handleChange}
+                />
+                <ErrorMessage
+                  name="confirmPassword"
+                  component="div"
+                  className="invalid-feedback"
+                />
+              </div>
+
+              <div class="footer">
                 <div className="checkbox">
                   <label>
                     <Field
@@ -236,15 +244,14 @@ function App(props) {
                           : '')
                       }
                       onKeyUp={handleChange}
-                      value=""
                     />
                     I have read and agree to <a href="#">Terms & Conditions</a>
                     <span className="checkmark"></span>
                     <ErrorMessage
-                    name="acceptTerms"
-                    component="div"
-                    className="invalid-feedback accept"
-                  />
+                      name="acceptTerms"
+                      component="div"
+                      className="invalid-feedback accept"
+                    />
                   </label>
                 </div>
 
@@ -254,7 +261,7 @@ function App(props) {
                     Sign up
                   </button>
                   <button
-                    type="button"
+                    type='button'
                     onClick={resetForm}
                   >
                     Reset
@@ -264,12 +271,13 @@ function App(props) {
                 <div className='redirect'>
                   <p>Already Have account? <a href="#">Login</a></p>
                 </div>
-              </Form>
-            )}
-          </Formik>
-        </div>
+              </div>
+            </Form>
+          )}
+        </Formik>
       </div>
-    );
+    </div>
+  );
 }
 
 $(function () {
